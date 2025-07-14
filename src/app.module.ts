b819c37258a4b5fc6e2ff03074b5ffff2b15a957
Sultan Room <sultanroomcode@gmail.com>
2025-07-14 20:52:31 +0700

    add contact module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { ErrorFilter } from './common/error.filter';
+import { ContactModule } from './contact/contact.module';
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [CommonModule, UserModule, ContactModule],
   controllers: [],
   providers: [ErrorFilter],
 })
