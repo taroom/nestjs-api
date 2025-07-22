6d6ef679e3afc299efd2f25661395e3bf4482b0a
Sultan Room <sultanroomcode@gmail.com>
2025-07-22 23:31:11 +0700

    add contact module components

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,6 +1,25 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { ContactService } from "./contact.service";
+import { User } from "@prisma/client";
+import { ContactResponse, CreateContactRequest } from "src/model/contact.model";
+import { request } from "http";
+import { Auth } from "src/common/auth.decorator";
+import { WebResponse } from "src/model/web.model";
 
-@Controller()
+@Controller('/api/contacts')
 export class ContactController {
+    // memasukkan depedency lewat constructor
+    constructor(private contactService: ContactService) { }
 
+    @Post()
+    @HttpCode(200)
+    async create(
+        @Auth() user: User,
+        @Body() request: CreateContactRequest
+    ): Promise<WebResponse<ContactResponse>> {
+        const result = await this.contactService.create(user, request);
+        return {
+            data: result
+        };
+    }
 }
--- src/contact/contact.validation.ts
+++ src/contact/contact.validation.ts
@@ -1,1 +1,10 @@
-export class ContactValidation { }
+import z, { ZodType } from "zod";
+
+export class ContactValidation {
+    static readonly CREATE: ZodType = z.object({
+        first_name: z.string().min(1).max(100),
+        last_name: z.string().min(1).max(100).optional(),
+        email: z.string().min(5).max(100).email().optional(),
+        phone: z.string().min(1).max(20).optional()
+    })
+}
