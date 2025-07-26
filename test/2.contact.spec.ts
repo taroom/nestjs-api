e781ae8ab85ba7aa1240645010e07f16d2315c55
Sultan Room <sultanroomcode@gmail.com>
2025-07-26 22:26:34 +0700

    add tests for retrieving contacts by ID in ContactController

--- test/2.contact.spec.ts
+++ test/2.contact.spec.ts
@@ -73,8 +73,46 @@ describe('ContactController', () => {
     });
   });
 
-  afterAll(async () => {
-    await testService.deleteContact();
-    await testService.deleteUser();
+  describe("GET /api/contacts/:contactId", () => {
+    beforeEach(async () => {
+      await testService.deleteContact();
+      await testService.deleteUser();
+
+      await testService.createUser();
+      await testService.createContact();
+    });
+
+    it("should be rejected if contact is not found", async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .get(`/api/contacts/${contact.id + 1}`)
+        .set('authorization', 'test');
+
+      logger.info(response.body);
+
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it("should be able to get contact where id is right", async () => {
+      const contact = await testService.getContact();
+
+      const response = await request(app.getHttpServer())
+        .get(`/api/contacts/${contact.id}`)
+        .set('authorization', 'test');
+      console.log(contact, response);
+      expect(response.status).toBe(200);
+      expect(response.body.data.first_name).toBe('Agus');
+      expect(response.body.data.last_name).toBe('Sutarom');
+      expect(response.body.data.email).toBe('taroom@gmail.com');
+      expect(response.body.data.phone).toBe('08123456789');
+      expect(response.body.data.id).toBeDefined();
+    });
+
+    afterAll(async () => {
+      await testService.deleteContact();
+      await testService.deleteUser();
+    });
   });
 });
