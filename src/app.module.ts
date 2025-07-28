c05d19c9de5ba4feabbd2731900270fc7e99e13d
Sultan Room <sultanroomcode@gmail.com>
2025-07-28 23:49:51 +0700

    registrasi address module di app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { ErrorFilter } from './common/error.filter';
 import { ContactModule } from './contact/contact.module';
+import { AddressModule } from './address/address.module';
 @Module({
-  imports: [CommonModule, UserModule, ContactModule],
+  imports: [CommonModule, UserModule, ContactModule, AddressModule],
   controllers: [],
   providers: [ErrorFilter],
 })
